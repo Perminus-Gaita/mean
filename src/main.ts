a6b3700a160f982d679ab5010c0addc37c200f9a
Perminus-Gaita <35493600+Perminus-Gaita@users.noreply.github.com>
2018-11-01 11:57:28 +0300

    Add explanation 5

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,15 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+/*
+To bootstrap our module based application, we need to inform Angular 
+which one is our root module to perform the compilation in the browser.
+This is done by declaring the root module inside the "bootstrapModule()" 
+function: platformBrowserDynamic().bootstrapModule(AppModule);
+This compilation in the browser is also known as "Just in Time" (JIT) compilation.+
+
+It is also possible to perform the compilation as a build step of our workflow. 
+This method is called "Ahead of Time" (AOT) compilation and will require a 
+slightly different bootstrap process.
+*/
