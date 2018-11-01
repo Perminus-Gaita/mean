f614aabfd6b7b890aa784aad459a5e09e607829b
Perminus-Gaita <35493600+Perminus-Gaita@users.noreply.github.com>
2018-11-01 14:08:35 +0300

    Added explanation 8.1

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -24,6 +24,10 @@ We are using a property called exports. We should only export
 whatever the other modules in our application need to perform its job. 
 In our case, we only need to make the RouterModule visible because 
 it's being used to direct links to certain paths.
+
+When a module is lazy loaded, Angular is going to create a child injector
+(which is a child of the root injector from the root module) and will 
+create an instance of our service there.
 */
 
 @NgModule({
