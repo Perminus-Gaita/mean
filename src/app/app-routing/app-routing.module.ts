a087740a91d4c98642a8117f7473efa15b9b01f4
Perminus-Gaita <35493600+Perminus-Gaita@users.noreply.github.com>
2018-11-02 04:48:42 +0300

    Added explanation 8.4

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -13,6 +13,18 @@ const routes: Routes = [{
   path: 'admin',
   loadChildren: 'app/admin/admin.module#AdminModule'
 }];
+/*
+The Angular Providers allow you to define set of Dependencies and
+provides the concrete, runtime version of that dependencies when asked for it.
+The Angular Components or services declare the dependencies they need in their 
+constructor. The Injector reads the dependencies and looks for them in the Providers.
+The Providers then instantiates the dependencies and provides them to the injector. 
+The Injector then injects them into the Components/Services.
+To Provide an instance of the service, the Angular Providers needs to know where to 
+find the provider and how to instantiates it.
+We let angular know that, we have a dependency to be injected by registering the 
+dependency with the Providers.
+*/
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
