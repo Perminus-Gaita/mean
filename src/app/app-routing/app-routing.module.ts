41ae5382f4124c379e02029c8ab3c77a23619a06
Perminus-Gaita <35493600+Perminus-Gaita@users.noreply.github.com>
2018-11-03 13:48:32 +0300

    Added .forRoot explanation

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -24,6 +24,24 @@ To Provide an instance of the service, the Angular Providers needs to know where
 find the provider and how to instantiates it.
 We let angular know that, we have a dependency to be injected by registering the 
 dependency with the Providers.
+
+          .forRoot()
+It has to do with singletons. Angular services are loaded onto the page 1 time 
+(singleton) and all references point back to this 1 instance.There is a risk that 
+a lazy loaded module will try to create a 2nd instance of what should be a singleton, 
+and the forRoot() method is a way to ensure that the app module / shared module / and 
+any lazy loaded module all use the same 1 instance (the root instance).
+
+The best approach is to create module with providers. Keep in mind that if your service 
+is in shared module, you may get multiple instances of it. Best idea then is to configure 
+module with Module.forRoot.
+By convention, the forRoot static method both provides and configures services at the 
+same time. It takes a service configuration object and returns a ModuleWithProviders.
+
+Call forRoot only in the root application module, AppModule. Calling it in any other module, 
+particularly in a lazy loaded module, is contrary to the intent and is likely to produce a runtime error.
+
+Remember to import the result; don't add it to any other @NgModule list.
 */
 
 @NgModule({
