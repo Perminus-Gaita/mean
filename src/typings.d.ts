338cea02be1dab7938221e31411ac514676ccfec
Perminus-Gaita <35493600+Perminus-Gaita@users.noreply.github.com>
2018-11-08 20:29:14 +0300

    Added interface explanation 2

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,5 +1,8 @@
 /* SystemJS module definition */
-/*Interfaces are used to define the shape of an object*/
+/*
+Interfaces are used to define the shape of an object.
+Interfaces are purely for declaration they cannot include any implementation.
+*/
 declare var module: NodeModule;
 interface NodeModule {
   id: string;
