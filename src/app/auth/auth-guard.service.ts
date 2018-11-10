18073538d441ed41b3af0466f2e9576e67c0f570
Perminus-Gaita <35493600+Perminus-Gaita@users.noreply.github.com>
2018-11-10 07:55:55 +0300

    Added services are classes explanation

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -1,6 +1,18 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 
+/*
+Services are classes just like components, 
+          @Component = Angular Component
+          @NgModule = Angular Module
+          @Injectable = Angular Service
+The visibility modifiers like: private, public and protected add the constructor argurments to member variables making them 
+available throughout the whole class.
+
+          Service Injection Context
+The list of services available to all components in your application. It is a shared space where all the components can access
+the services available as ling as they are somehow inported into the root module
+*/
 @Injectable()
 export class AuthGuard implements CanActivate {
 
