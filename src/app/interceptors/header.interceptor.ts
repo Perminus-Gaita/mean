d1d2a75f0a709751a2d5b5091a126707d108674e
Perminus-Gaita <35493600+Perminus-Gaita@users.noreply.github.com>
2018-11-26 13:20:04 +0300

    Added set method explanation

--- src/app/interceptors/header.interceptor.ts
+++ src/app/interceptors/header.interceptor.ts
@@ -19,6 +19,26 @@ export class AuthHeaderInterceptor implements HttpInterceptor {
 }
 
 /*
+>>set() method (line 13)
+The set() method of the Headers interface sets a new value for an existing header inside a Headers object, 
+or adds the header if it does not already exist.
+The difference between set() and Headers.append is that if the specified header already exists and accepts 
+multiple values, set() overwrites the existing value with the new one, whereas Headers.append appends the 
+new value to the end of the set of values.
+For security reasons, some headers can only be controller by the user agent. These headers include the 
+forbidden header names  and forbidden response header names.
+
+Syntax : myHeaders.set(name,value);
+name
+The name of the HTTP header you want to set to a new value. If the given name is not the name of an HTTP header, 
+this method throws a TypeError.
+value
+The new value you want to set.
+Returns
+Void.
+
+....................................
+
 A HTTP Interceptor enables you to catch HTTP requests and responses so that you can modify them. 
 It is very useful especially when you want to add extra headers to all outgoing requests or catch error responses from the server.
 
